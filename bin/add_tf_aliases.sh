8f098c5ce1aa82f5318a6a2cc741e421ad4efaa8
Susanta Chattopadhyay <itssusanta@gmail.com>
2023-09-27 22:40:58 -0400

    15 tf alias for terraform (#16)
    * #15 - corrected path for bash script
    
    * #15 - added script to add tf alias to .bash_profile

---
+++ bin/add_tf_aliases.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Define the alias and the Terraform command
+alias_name="tf"
+terraform_command="terraform"
+
+# Check if the alias is already defined in .bash_profile
+if ! grep -q "alias $alias_name='$terraform_command'" ~/.bash_profile; then
+    # If the alias doesn't exist, add it to .bash_profile
+    echo "alias $alias_name='$terraform_command'" >> ~/.bash_profile
+    echo "Alias '$alias_name' for '$terraform_command' added to ~/.bash_profile."
+    echo "You may need to restart your terminal or run 'source ~/.bash_profile' for changes to take effect."
+else
+    echo "Alias '$alias_name' for '$terraform_command' already exists in ~/.bash_profile."
+fi

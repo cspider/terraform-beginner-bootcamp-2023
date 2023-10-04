5a1b59325086e25285c1ed2e80371640ebfe98be
Susanta Chattopadhyay <itssusanta@gmail.com>
2023-10-03 22:08:48 -0400

    #30 - implemented CDN (#31)

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -1,38 +1,23 @@
+terraform {
+    required_providers {
+      
+      aws = {
+        source = "hashicorp/aws"
+        version = "5.18.1"
+      }
+    }
+}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity            
+data "aws_caller_identity" "current" {}
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
-  provider = aws
-
-  tags = {
-    UserUuid   = var.user_uuid
-  }
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
-resource "aws_s3_object" "index_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "index.html"
-  source = var.index_html_filepath
-  etag   = filemd5("${path.root}/public/index.html")
-}
-resource "aws_s3_object" "error_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "error.html"
-  source = var.error_html_filepath
-  etag   = filemd5("${path.root}/public/error.html")
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
-resource "aws_s3_bucket_website_configuration" "website_configuration" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
 }
-
